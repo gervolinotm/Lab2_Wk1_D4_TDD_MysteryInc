21ae63a1da20bef5309d46cb9b87e5000685c21d
Thomas Gervolino <thomas_gervolino@hotmail.com>
2019-04-04 15:01:32 +0100

    Completed 2 and 3.

---
+++ friends.rb
@@ -0,0 +1,11 @@
+def get_name(person)
+  return person[:name]
+end
+
+def favourite_tv_show(person)
+  return person[:favourites][:tv_show]
+end
+
+def likes_to_eat(person, snacks)
+  return true if @person4[:favourites][:snacks]
+end
